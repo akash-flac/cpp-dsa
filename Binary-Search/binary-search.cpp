06015e348e5447f8f48e5714e6073bce7f5fa0ed
monk <akash-flac@github.com>
2023-03-13 19:10:17 +0530

    commit

--- Binary-Search/binary-search.cpp
+++ Binary-Search/binary-search.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int binarysearch(int ar[], int size, int key)
 {   
-    int s = ar[0];
-    int e = ar[size-1];
+    int s = 0;
+    int e = size-1;
     int mid = (s+e)/2 ;
     while(s <= e)
     { 
@@ -23,7 +23,7 @@ int binarysearch(int ar[], int size, int key)
             s = mid + 1;
         }
 
-        mid = (s+e)/2;
+        int mid = (s+e)/2;
     }
 
     return -1; 
@@ -31,6 +31,7 @@ int binarysearch(int ar[], int size, int key)
 
 int main()
 {
-   int arr[] = {1, 5, 7, 9, 11};
-   cout << " Index of the given elemnt is " << binarysearch(arr, 5, 7);
+   int arr[] = {1, 5, 7, 9, 11};//15, 20, 45, 50, 52, 58, 60, 70, 85, 100};
+   cout << "mai to chal raha hi";
+   cout << " Index of the given elemnt is " << binarysearch(arr, 5, 7) << endl;
 }
